15f2b0294dfec6b1583b1f12704d29384e976d5c
panxiubin <xiubinpan@gmail.com>
2017-03-23 19:27:22 +0800

    rspec

--- leap_year.rb
+++ leap_year.rb
@@ -1,11 +1,20 @@
-def is_leap_year?(year)
-  if year % 4 == 0
-    if year % 100 == 0
-      return false
-    else
-      return true
-    end
-  else
-    return false
-  end
+# def is_leap_year?(year)
+#   if year % 4 == 0
+#     if year % 100 == 0
+#       if year % 400 == 0
+#         return true
+#       else
+#         return false
+#       end
+#     else
+#       return true
+#     end
+#   else
+#     return false
+#   end
+# end
+
+
+def is_leap_year?(y)
+  y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)
 end
